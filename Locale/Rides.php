52686bdf4abfc0fe04aa46976d21db32f2c59096
Malik Naik <maliknaik16@gmail.com>
2019-06-11 02:57:17 +0530

    Update Rides.php

--- Locale/Rides.php
+++ Locale/Rides.php
@@ -13,16 +13,26 @@ namespace Locale;
 class Rides {
   /**
    * The database connection
+   *
+   * @var \Locale\Database
    */
   protected $db_connection;
 
   /**
    * Name of the table where rides data is stored
+   *
+   * @var string
    */
   protected $table_name;
 
   /**
    * Initializes the database connection
+   *
+   * @param \Locale\Database $database
+   *  Database object
+   * @param string $table_name
+   *  Name of the table
+   *
    */
   public function __construct(Database $database, $table_name) {
     $this->db_connection = $database;
@@ -31,6 +41,15 @@ class Rides {
 
   /**
    * Returns the Latitude and Longitude
+   *
+   * @param string $lat_column
+   *  Name of the latitude field
+   * @param string $long_column
+   *  Name of the longituede field
+   * @param int $id
+   *  ID of the row
+   *
+   * @return array
    */
   public function getLatLong($lat_column, $long_column, $id) {
     $query = "SELECT {$lat_column}, {$long_column} FROM {$this->table_name} WHERE id={$id}";
@@ -44,6 +63,13 @@ class Rides {
 
   /**
    * Returns the rides by the travel type
+   *
+   * @param int $travel_type_id
+   *  Type of travel (1=long distance, 2= point to point, 3= hourly rental).
+   * @param array $fields
+   *  Columns/Fields of the table
+   *
+   * @return array
    */
   public function getRidesByTravelType($travel_type_id, $fields = array()) {
     $c = '*';
@@ -63,6 +89,11 @@ class Rides {
 
   /**
    * Returns the rides travelled by the user
+   *
+   * @param int $user_id
+   *  User id of the user
+   *
+   * @return array
    */
   public function getRidesByUserId($user_id) {
     $result = array();
@@ -80,6 +111,9 @@ class Rides {
 
   /**
    * Returns the cancelled rides
+   *
+   * @return array
+   *  Array of cancelled rides
    */
   public function getRidesCancelled() {
     $result = array();
@@ -97,6 +131,9 @@ class Rides {
 
   /**
    * Returns the rides other than cancelled rides
+   *
+   * @return array
+   *  Array of not cancelled rides
    */
   public function getRidesNotCancelled() {
     $result = array();
@@ -114,6 +151,11 @@ class Rides {
 
   /**
    * Returns the rides by the vehicle model id
+   *
+   * @param int $vehicle_model_id
+   *  ID of the Vehicle model
+   *
+   * @return array
    */
   public function getRidesByVehicleModelId($vehicle_model_id) {
     $result = array();
@@ -131,6 +173,11 @@ class Rides {
 
   /**
    * Returns all the rides
+   *
+   * @param array $fields
+   *  Columns/Fields of the table
+   *
+   * @return array
    */
   public function getAllRides($fields = array()) {
     $result = array();
